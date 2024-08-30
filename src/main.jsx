3f6922bfba5341f451dcbbc1d3d46e8d81eead74
FIRST_NAME LAST_NAMEAnasPirzada <anaspirzadaiub@gmail.com>
2024-08-30 15:33:59 +0500

    font file added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './fonts/Gilroy-Bold.ttf';
+import './fonts/Gilroy-ExtraBold.ttf';
+import './fonts/Gilroy-Light.ttf';
+import './fonts/Gilroy-Medium.ttf';
+import './fonts/Gilroy-Regular.ttf';
+import './fonts/Gilroy-SemiBold.ttf';
+import './fonts/Gilroy-Thin.ttf';
+import './index.css';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
