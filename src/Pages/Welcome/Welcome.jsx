6022e44cc570ee5b81277b27c5393967e6986457
FIRST_NAME LAST_NAMEAnasPirzada <anaspirzadaiub@gmail.com>
2024-09-09 18:47:29 +0500

    welcome page size decreased

--- src/Pages/Welcome/Welcome.jsx
+++ src/Pages/Welcome/Welcome.jsx
@@ -57,7 +57,7 @@ export const Welcome = () => {
 
         <div className="bg-[url('/blurybg.png')] relative z-10 flex flex-col items-center justify-center w-full h-full bg-opacity-50">
           <div>
-            <h1 className=' text-5xl lg:text-8xl  drop-shadow-[0_4px_4px_rgba(255,255,255,1)] font-bold Gilroy-Bold text-white lowercase'>
+            <h1 className=' text-5xl lg:text-8xl font-bold Gilroy-Bold text-white lowercase'>
               welcome home{' '}
             </h1>
           </div>
@@ -70,7 +70,7 @@ export const Welcome = () => {
               <motion.button
                 whileTap={{ scale: 0.985 }}
                 ref={btnRef}
-                className='relative w-full max-w-xs overflow-hidden border-2 border-[#EC9047CC] rounded-full bg-transparent drop-shadow-[0px_2px_24px_0px_#00000040] px-10 py-3 Gilroy-Regular font-normal text-3xl mt-16 md:mt-0 text-white group transition-colors duration-300 ease-in-out'
+                className='relative w-full max-w-xs overflow-hidden border-2 border-[#EC9047CC] rounded-full bg-transparent drop-shadow-[0px_2px_24px_0px_#00000040] px-6 py-2 Gilroy-Regular font-normal text-3xl mt-16 md:mt-0 text-white group transition-colors duration-300 ease-in-out'
               >
                 Get started
                 <span
